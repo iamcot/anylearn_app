414224dde1bfaaf0058420c9ba1a1d6bcf0e7f43
ngohieuphat <ngohipha1@gmail.com>
2022-09-27 15:54:43 +0700

    Al-76"locale"

--- lib/screens/locale.dart
+++ lib/screens/locale.dart
@@ -1,13 +1,44 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LocaleScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _LocaleScreen();
+  final locale = Locale("vi", "en");
 }
 
 class _LocaleScreen extends State<LocaleScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chọn ngôn ngữ"),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              TextButton.icon(
+                icon: Icon(Icons.language),
+                onPressed: () {
+                  context.locale = Locale('vi');
+
+                  print(context.supportedLocales);
+                },
+                label: Text('Việt Nam'),
+              ),
+              TextButton.icon(
+                icon: Icon(Icons.language),
+                onPressed: () {
+                  context.locale = Locale ('en');
+                },
+                label: Text('English'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
